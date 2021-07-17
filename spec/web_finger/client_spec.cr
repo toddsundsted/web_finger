3c140fdca1f5f3edeaeabac368c13aacd596922f
Todd Sundsted <todd@sundsted.com>
2021-07-17 09:33:59 -0400

    Fix failing test in Crystal 1.1.0.

--- spec/web_finger/client_spec.cr
+++ spec/web_finger/client_spec.cr
@@ -32,7 +32,11 @@ class HTTP::Client
     url = url.is_a?(String) ? URI.parse(url) : url
     @@history << url
     if url.host =~ /does-not-exist/
-      raise Socket::Addrinfo::Error.new(LibC::EAI_NONAME, "No address found", url.host)
+      {% if compare_versions(Crystal::VERSION, "1.1.0") >= 0 %}
+        raise Socket::Addrinfo::Error.from_os_error(nil, Errno.new(LibC::EAI_NONAME), domain: url.host, type: Socket::Type::STREAM, protocol: Socket::Protocol::TCP, service: 80)
+      {% else %}
+        raise Socket::Addrinfo::Error.new(LibC::EAI_NONAME, "No address found", url.host)
+      {% end %}
     end
     case url.query || url.path
     when /redirect/
